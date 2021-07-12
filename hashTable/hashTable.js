9146cf6b6a5f14ae53d29c97f95dd3d0112ce431
Angel Ricardo Vega Hernandez <angelricardo.uthh@gmail.com>
2021-07-12 13:42:25 -0500

    HashTable

---
+++ hashTable/hashTable.js
@@ -0,0 +1,43 @@
+// JavaScript source code
+class MyHashTable {
+    constructor(size) {
+        this.data = new Array(size);
+    }
+
+    hashMethod(key) {
+        let hash = 0;
+        for (let i = 0; i < key.length; i++) {
+            hash = (hash + key.charCodeAt(i) * i) % this.data.length;
+        }
+        return hash;
+    }
+
+    set(key, value) {
+        const address = this.hashMethod(key);
+        if (!this.data[address]) {
+            this.data[address] = [];
+        }
+        this.data[address].push([key, value]);
+        return this.data;
+    }
+
+    get(key) {
+        const address = this.hashMethod(key);
+        const currentBucket = this.data[address];
+        if (currentBucket) {
+            for (let i = 0; i < currentBucket.length; i++) {
+                if (currentBucket[i][0] === key) {
+                    return currentBucket[i][1];
+                }
+
+            }
+        }
+        return undefined;
+    }
+}
+
+let myHashTable = new MyHashTable(50);
+myHashTable.set("Mariana", 1990);
+myHashTable.set("Jose", 1991);
+myHashTable.set("Dani", 1989);
+myHashTable.get("Jose");
